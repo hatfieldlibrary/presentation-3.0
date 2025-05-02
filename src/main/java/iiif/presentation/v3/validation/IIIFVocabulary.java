f75c8dca10995fd9dd7c6bdd0b28529f0a0e01dc
Michael Spalti <mspalti@gmail.com>
2025-05-02 16:18:26 -0700

    Added behavior validation for Collection

--- src/main/java/iiif/presentation/v3/validation/IIIFVocabulary.java
+++ src/main/java/iiif/presentation/v3/validation/IIIFVocabulary.java
@@ -3,8 +3,6 @@ package iiif.presentation.v3.validation;
 import iiif.presentation.v3.behavior.Behavior;
 import java.util.List;
 
-import static iiif.presentation.v3.behavior.Behavior.validManifestBehaviors;
-
 public class IIIFVocabulary {
 
     public static boolean isValidBehaviorCombination(String type, List<String> behaviors) {
@@ -16,9 +14,16 @@ public class IIIFVocabulary {
             if (!isValidBehavior(behavior))
                 return false;
         }
+        if ("Collection".equals(type)) {
+            for (String behavior: behaviors){
+                if (!Behavior.validCollectionBehaviors().contains(behavior)){
+                    return false;
+                }
+            }
+        }
         if ("Manifest".equals(type)) {
             for (String behavior: behaviors){
-                if (!validManifestBehaviors().contains(behavior)){
+                if (!Behavior.validManifestBehaviors().contains(behavior)){
                     return false;
                 }
             }
